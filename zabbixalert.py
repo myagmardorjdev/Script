f6f8ce4fe4414e695f82f3fa1756d5887428bfe6
myagmardorjdev <developer@altanjoloo.mn>
2023-07-28 15:25:25 +0800

    problem uudaa dict руу оруулав.

--- zabbixalert.py
+++ zabbixalert.py
@@ -2,10 +2,62 @@ import requests
 import json
 from requests.auth import HTTPBasicAuth
 
+def initsession():
+    url = 'http://10.0.0.14/apirest.php/initSession'
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'user_token gbx24bStlmOLnf7PxANoU6h9KgaPSpEeOeogmqWW',
+        'App-Token': 'dgoT8fOH3UYbYV1bz49N2PrFUhKY6Bqp8bJgrGRP'
+    }
+    response = requests.get(url, headers=headers)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        data = response.json()
+        return data['session_token']
+    else:
+        print("Error:", response.status_code)
+def createticket(session_token):
+    url = 'http://10.0.0.14/apirest.php/Ticket/'
+
+    # Replace 'your_username' and 'your_password' with your actual GLPI credentials
+    username = 'myagmardorj'
+    password = 'Az123456!0@'
+    headers = {
+        'Content-Type': 'application/json',
+        'App-Token': 'dgoT8fOH3UYbYV1bz49N2PrFUhKY6Bqp8bJgrGRP',
+        'Session-Token' : session_token
+    }
+    # Data for creating the ticket (replace with actual data)
+    ticket_data = {
+        'input': {
+            'name': 'Your Ticket Title',
+            'content': 'Your ticket description goes here',
+            'priority': 3,  # Replace with the appropriate priority level
+            'itemtype': 'Ticket',  # If you are creating a ticket
+            # Add other relevant data based on your GLPI setup and requirements
+        }
+    }
+
+    try:
+        response = requests.post(url, json=ticket_data,headers=headers, auth=HTTPBasicAuth(username, password))
+
+        # Check if the request was successful (status code 201 for created)
+        if response.status_code == 201:
+            data = response.json()
+            ticket_id = data['id']
+            print(f"Ticket created successfully with ID: {ticket_id}")
+        else:
+            print(f"Failed to create ticket. Status code: {response.status_code}")
+            print(response.json())
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
 
 ZABBIX_API_URL = "https://zbx.altanjoloo.mn/zabbix/api_jsonrpc.php"
 UNAME = "Myagmardorj"
-PWORD = "Aa1234567"
+PWORD = "Az123456!0@"
 AUTHTOKEN = "56bc1ff73deb2c79145b1e8315a064e8e967251e2d4b60f0d1207c077ea2a5ef"
 
 
@@ -45,7 +97,7 @@ jarray = r2.json()["result"]
 print("Төрөл: ",type(jarray))
 print("Урт: ", len(jarray))
 #Possible values:0 - not classified;1 - information;2 - warning;3 - average;4 - high;5 - disaster.
-severvalue = 3
+severvalue = 4
 counter=0
 for i in range(len(jarray)):
     tmp = int(jarray[i]['severity'])
@@ -70,64 +122,10 @@ for i in range(len(jarray)):
         print(json.dumps(r3.json(), indent=5, sort_keys=True))
    
 print(counter)
-#glpitoker - NckBv0PbYGQJ5sK1YEDVmwHPjKOlt58WbBQbgjKg
-
-def initsession():
-    url = 'http://10.0.0.14/apirest.php/initSession'
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'user_token gbx24bStlmOLnf7PxANoU6h9KgaPSpEeOeogmqWW',
-        'App-Token': 'dgoT8fOH3UYbYV1bz49N2PrFUhKY6Bqp8bJgrGRP'
-    }
-    response = requests.get(url, headers=headers)
-
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        data = response.json()
-        return data['session_token']
-    else:
-        print("Error:", response.status_code)
+#
 
 
-sessiontoken = initsession()
+#sessiontoken = initsession()
 
-def createticket(session_token):
-    url = 'http://10.0.0.14/apirest.php/Ticket/'
-
-    # Replace 'your_username' and 'your_password' with your actual GLPI credentials
-    username = 'myagmardorj'
-    password = 'Az123456!0@'
-    headers = {
-        'Content-Type': 'application/json',
-        'App-Token': 'dgoT8fOH3UYbYV1bz49N2PrFUhKY6Bqp8bJgrGRP',
-        'Session-Token' : session_token
-    }
-    # Data for creating the ticket (replace with actual data)
-    ticket_data = {
-        'input': {
-            'name': 'Your Ticket Title',
-            'content': 'Your ticket description goes here',
-            'priority': 3,  # Replace with the appropriate priority level
-            'itemtype': 'Ticket',  # If you are creating a ticket
-            # Add other relevant data based on your GLPI setup and requirements
-        }
-    }
-
-    try:
-        response = requests.post(url, json=ticket_data,headers=headers, auth=HTTPBasicAuth(username, password))
-
-        # Check if the request was successful (status code 201 for created)
-        if response.status_code == 201:
-            data = response.json()
-            ticket_id = data['id']
-            print(f"Ticket created successfully with ID: {ticket_id}")
-        else:
-            print(f"Failed to create ticket. Status code: {response.status_code}")
-            print(response.json())
-
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-
-
-createticket(sessiontoken)
+#createticket(sessiontoken)
 

1fad1d003d53557d6d1a36f58447091b3d89b3c4
myagmardorjdev <developer@altanjoloo.mn>
2023-07-31 16:57:49 +0800

    while loopend оруулав

--- zabbixalert.py
+++ zabbixalert.py
@@ -1,10 +1,8 @@
 import requests
 import json
+import time
 from requests.auth import HTTPBasicAuth
 from datetime import datetime
-now = datetime.now()
-
-createdtickets = {}
 
 def initsession():
     url = 'http://10.0.0.14/apirest.php/initSession'
@@ -58,6 +56,7 @@ def createticket(session_token,title,content):
     except requests.exceptions.RequestException as e:
         print(f"An error occurred: {e}")
 def get_hosts_with_ip(hostid):
+
     headers = {
         'Content-Type': 'application/json'
     }
@@ -79,111 +78,110 @@ def get_hosts_with_ip(hostid):
     response = requests.post(ZABBIX_API_URL, json=payload, headers=headers)
     return response.json()['result'][0]['interfaces'][0]['ip']
 
+#loop outsides variables
+createdtickets = {}
 
-ZABBIX_API_URL = "https://zbx.altanjoloo.mn/zabbix/api_jsonrpc.php"
-UNAME = "Myagmardorj"
-PWORD = "Az123456!0@"
-AUTHTOKEN = "56bc1ff73deb2c79145b1e8315a064e8e967251e2d4b60f0d1207c077ea2a5ef"
+while(1==1):
+    now = datetime.now()
+    if now.hour > 8 and now.hour < 24:
+        ZABBIX_API_URL = "https://zbx.altanjoloo.mn/zabbix/api_jsonrpc.php"
+        UNAME = "Myagmardorj"
+        PWORD = "Az123456!0@"
+        AUTHTOKEN = "56bc1ff73deb2c79145b1e8315a064e8e967251e2d4b60f0d1207c077ea2a5ef"
 
-r = requests.post(ZABBIX_API_URL,
-                  json={
-                      "jsonrpc": "2.0",
-                      "method": "user.login",
-                      "params": {
-                          "user": UNAME,
-                          "password": PWORD},
-                      "id": 1
-                  })
+        r = requests.post(ZABBIX_API_URL,
+                        json={
+                            "jsonrpc": "2.0",
+                            "method": "user.login",
+                            "params": {
+                                "user": UNAME,
+                                "password": PWORD},
+                            "id": 1
+                        })
 
-print(json.dumps(r.json(), indent=4, sort_keys=True))
-AUTHTOKEN = r.json()["result"]
-print("\nRetrieve a list of problems")
-r2 = requests.post(ZABBIX_API_URL,
-                json={     
-                    "jsonrpc": "2.0",     
-                    "method":"problem.get",     
-                    "params": {         
-                          #"output": "extend",         "selectDServices": "extend",         "druleids": "19"
-                            "output": "extend",
-                            "selectHosts": ["host", "name",'allowed_hosts','hostid'],
-                            "interfaces" : ['ip','allowed_hosts','hostid'],
-                            "selectAcknowledges": "extend",
-                            "recent": "true",
-                            "sortfield": ["eventid"],
-                            "sortorder": "ASC",
-                            "severity": 5    
-                        }  ,
-                    
-                        "id": 2 ,
-                        "auth": AUTHTOKEN
-                  })
-#print(json.dumps(r2.json(), indent=4, sort_keys=True))
+        AUTHTOKEN = r.json()["result"]
+        r2 = requests.post(ZABBIX_API_URL,
+                        json={     
+                            "jsonrpc": "2.0",     
+                            "method":"problem.get",     
+                            "params": {         
+                                #"output": "extend",         "selectDServices": "extend",         "druleids": "19"
+                                    "output": "extend",
+                                    "selectHosts": ["host", "name",'allowed_hosts','hostid'],
+                                    "interfaces" : ['ip','allowed_hosts','hostid'],
+                                    "selectAcknowledges": "extend",
+                                    "recent": "true",
+                                    "sortfield": ["eventid"],
+                                    "sortorder": "ASC",
+                                    "severity": 5    
+                                }  ,
+                            
+                                "id": 2 ,
+                                "auth": AUTHTOKEN
+                        })
+        #print(json.dumps(r2.json(), indent=4, sort_keys=True))
+
+        sessiontoken = initsession()
+        jarray = r2.json()["result"]
+        #Possible values:0 - not classified;1 - information;2 - warning;3 - average;4 - high;5 - disaster.
+        severvalue = 4
+        counter=0
+        ustgax_day_limit = 2
+        allproblemdict = {} 
+        for i in range(len(jarray)):
+            tmp = int(jarray[i]['severity'])
+            if tmp > severvalue:
+                r3 = requests.post(ZABBIX_API_URL,
+                            json={     
+                                "jsonrpc": "2.0",     
+                                "method":"event.get",     
+                                "params": {         
+                                        "output": ['eventid', 'hostid','name','clock','severity','ip'],
+                                        'selectHosts': ['hostid', 'host'],
+                                        "select_acknowledges": "extend",         
+                                        "selectTags": "extend",        
+                                        "eventids": jarray[i]['eventid']  
+                                    }  ,     
+                                    "id": 2 ,
+                                    "auth": AUTHTOKEN
+                            })
+                #print(json.dumps(r3.json(), indent=4, sort_keys=True))
+                #Client pos bolon not restarted 24 hours gdg iig algasaj bna 
+                if "Client" not in r3.json().get('result')[0].get('hosts')[0]['host'] and "last 24hours" not in  r3.json().get('result')[0]['name']:
+                    eachdict = {
+                        "host" : r3.json().get('result')[0].get('hosts')[0]['host'],
+                        "reason" : r3.json().get('result')[0]['name'],
+                        "ip_address" : get_hosts_with_ip( r3.json().get('result')[0].get('hosts')[0].get('hostid'))
+                    }
+                    counter+=1
+                    allproblemdict[counter] = eachdict
 
 
+        counter = len(createdtickets)
+        isfind = False
+        for x in allproblemdict:
+            # 2 dict харьцуулж байгаа үгйүг нь шалгаж байна
+            for z in createdtickets:
+                if allproblemdict[x]['host'] == createdtickets[z]['host'] and allproblemdict[x]['reason'] == createdtickets[z]['reason']:
+                    isfind = True
+                    break
 
-sessiontoken = initsession()
-jarray = r2.json()["result"]
-print("Төрөл: ",type(jarray))
-print("Урт: ", len(jarray))
-#Possible values:0 - not classified;1 - information;2 - warning;3 - average;4 - high;5 - disaster.
-severvalue = 4
-counter=0
-ustgax_day_limit = 2
-allproblemdict = {} 
-for i in range(len(jarray)):
-    tmp = int(jarray[i]['severity'])
-    if tmp > severvalue:
-        r3 = requests.post(ZABBIX_API_URL,
-                    json={     
-                        "jsonrpc": "2.0",     
-                        "method":"event.get",     
-                        "params": {         
-                                "output": ['eventid', 'hostid','name','clock','severity','ip'],
-                                'selectHosts': ['hostid', 'host'],
-                                "select_acknowledges": "extend",         
-                                "selectTags": "extend",        
-                                "eventids": jarray[i]['eventid']  
-                            }  ,     
-                            "id": 2 ,
-                            "auth": AUTHTOKEN
-                    })
-        #print(json.dumps(r3.json(), indent=4, sort_keys=True))
-        if "Client" not in r3.json().get('result')[0].get('hosts')[0]['host'] and "last 24hours" not in  r3.json().get('result')[0]['name']:
-            eachdict = {
-                "host" : r3.json().get('result')[0].get('hosts')[0]['host'],
-                "reason" : r3.json().get('result')[0]['name'],
-                "ip_address" : get_hosts_with_ip( r3.json().get('result')[0].get('hosts')[0].get('hostid'))
-            }
-            counter+=1
+            # host created ticket dotor baixgvi baiwal shineer vvsgeed, daraa ni vvssen ticket dotor nemj ogj bna
+            if isfind == False:
+                #createticket(sessiontoken,ticket,content)
+                print("ticket created   ---" , allproblemdict[x]['host'])
+                eachdict1 ={
+                    "host" : allproblemdict[x]['host'],
+                    "reason" : allproblemdict[x]['reason'],
+                    "day": now.day,
+                    "hour" : now.hour,
+                    "date" : now
+                }
+                createdtickets[counter] = eachdict1
+                counter+=1
+
+            isfind = False
         
-        allproblemdict[counter] = eachdict
-        
-
-print("counter: ", counter)
-counter = len(createdtickets)
-isfind = False
-for x in allproblemdict:
-    for z in createdtickets:
-        if allproblemdict[x]['host'] == createdtickets[z]['host']:
-            isfind = True
-            break
-
-    # host created ticket dotor baixgvi baiwal shineer vvsgeed, daraa ni vvssen ticket dotor nemj ogj bna
-    if isfind == False:
-        #createticket(sessiontoken,ticket,content)
-        print("ticket craeted   ---" , allproblemdict[x]['host'])
-        eachdict1 ={
-        "host" : allproblemdict[x]['host'],
-        "reason" : allproblemdict[x]['reason'],
-        "day": now.day,
-        "hour" : now.hour,
-        "date" : now
-        }
-        createdtickets[counter] = eachdict1
-        counter+=1
-        
-
-    isfind = False
-   
-    
-    
+            
+        time.sleep(4)
+    print("sleeping...")

894389cd37efe9239d763ee504b8e2db288e0b70
myagmardorjdev <developer@altanjoloo.mn>
2023-09-18 15:58:04 +0800

    Тасалсан цаг буруу гараад байгаа өмнөх

--- glpi_api.ps1
+++
@@ -1,5 +0,0 @@
-curl.exe -X GET \
--H 'Content-Type: application/json' \
--H "Authorization: user_token gbx24bStlmOLnf7PxANoU6h9KgaPSpEeOeogmqWW" \
--H "App-Token: dgoT8fOH3UYbYV1bz49N2PrFUhKY6Bqp8bJgrGRP" \
-'http://10.0.0.14/apirest.php/initSession'

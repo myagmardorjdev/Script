18fd55b3ac4d491f80f8596a6b82cbd620f11cfc
myagmardorjdev <developer@altanjoloo.mn>
2023-10-03 10:30:11 +0800

    pos_order table inner join d oruulaw

--- odoo_to_infinity/main.py
+++ odoo_to_infinity/main.py
@@ -24,13 +24,37 @@ def postg(conn,query):
 now = datetime.now()
 seconddate = now + timedelta(days=1)
 tdate = "'"+ str(now)[:10]+ "' and '" +str(seconddate)[:10] +"'"
-pos_orders_query = "SELECT pos.id,pos.company_id,pro.barcode,pos.name,pos.product_id,pos.price_unit,pos.qty,pos.price_subtotal,pos.price_subtotal_incl,pos.order_id,pos.full_product_name,pos.create_date FROM pos_order_line as pos inner join product_product pro on pos.product_id = pro.id WHERE pos.create_date between "+tdate
+                           #0    #1              #2          #3       #4            #5              #6      #7                #8                      #9            #10                   #11            #12              #13              #14             #15         #16 
+pos_orders_query = "SELECT pos.id,pos.company_id,pro.barcode,pos.name,pos.product_id,pos.price_unit,pos.qty,pos.price_subtotal,pos.price_subtotal_incl,pos.order_id,pos.full_product_name,pos.create_date,ord.amount_paid,ord.pos_reference,ord.employee_id,ord.cashier,ord.bill_id  FROM pos_order_line as pos inner join product_product pro on pos.product_id = pro.id inner join pos_order as ord on ord.id = pos.order_id WHERE pos.create_date between "+tdate
 # ? default variables
 default_config_dict = readtextfile_to_dict(file_running_directory+"configuration.txt").returnc()
 loop_status = 1
 loop_sleeptime = 2 #second
 odoodatabases = {'user': 'readonly_c34','password': 'readonly_c34_password','server': '10.34.1.220','port': 5432,'database':'CARREFOURS34_LIVE'}
 
+#loop inside
 conp = psycopg2.connect(database=odoodatabases['database'], user=odoodatabases['user'], password=odoodatabases['password'], host=odoodatabases['server'], port= odoodatabases['port'])
 qresult = postg(conp,pos_orders_query)
-print(qresult[0])
+print(qresult[0][3])
+billdict = {}
+SaleItems = {}
+SaleItems1 = {"Barcode":qresult[0][2],"Qty":float(qresult[0][6]),"ItemDueAmount":float(qresult[0][8])}
+SaleItems2 = {"Barcode":qresult[1][2],"Qty":float(qresult[1][6]),"ItemDueAmount":float(qresult[1][8])}
+
+SaleItems[0] = SaleItems1
+SaleItems[1] = SaleItems2
+
+
+formatted_output = {
+    "SaleItems": [
+        {
+            "Barcode": str(item['Barcode']),
+            "Qty": str(item['Qty']),
+            "ItemDueAmount": int(item['ItemDueAmount'])
+        }
+        for item in SaleItems.values()
+    ]
+}
+
+# Print the formatted output
+print(formatted_output)
